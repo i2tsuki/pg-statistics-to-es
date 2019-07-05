dcfc41d3f3279726df5ac26aec857313ea6aee0a
i2tsuki <mac-daisuki@live.jp>
2019-07-05 23:30:03 +0900

    Finally clean docker container

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 set -eu
 
 docker build --pull -t pg-statistics-to-es .
+set +e
 CONTAINER_ID=$(docker create pg-statistics-to-es)
 docker cp ${CONTAINER_ID}:/pg-statistics-to-es/dist ./
 docker rm ${CONTAINER_ID}
+set -e
